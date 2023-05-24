374e141416f36ff2ff4ae3981b96a716786bcb68
MampferBTW <mampferbtw@gmx.at>
2023-05-24 10:33:07 +0200

    Javascript explained

--- index.js
+++ index.js
@@ -26,3 +26,47 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+/*let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Function to display slides
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides"); // Get all elements with the class "mySlides"
+  let dots = document.getElementsByClassName("dot"); // Get all elements with the class "dot"
+  
+  // If the given index is greater than the number of slides, set the index to 1
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  
+  // If the given index is less than 1, set the index to the last slide
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  
+  // Hide all slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  // Remove the "active" class from all dots
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  // Display the current slide and add the "active" class to the corresponding dot
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}*/
